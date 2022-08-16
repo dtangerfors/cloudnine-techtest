1ef6694dce3a797d47d3a0e53af0eea259dcd18a
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-16 20:05:43 +0200

    Added Header component

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import Filter from '../components/Filter'
+
 import Header from '../components/Header'
+import Filter from '../components/Filter'
 import ListSalons from '../components/ListSalons'
 
 const List = () => {
 return (
    <>
-   <Header />
-   <main className="pt-[50px]">
-      <Filter />
-      <ListSalons />
-   </main>
+      <Header />
+      <main className="pt-[50px]">
+         <Filter />
+         <ListSalons />
+      </main>
    </>
 )
 }
