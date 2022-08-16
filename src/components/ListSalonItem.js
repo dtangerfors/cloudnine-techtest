16776e738533877df02ebba2332569ad1f601454
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-16 20:06:17 +0200

    Added Link component

--- src/components/ListSalonItem.js
+++ src/components/ListSalonItem.js
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import chevron_left from "../images/icons/chevron.left.svg"
 import Rating from "./Rating";
 
 const ListSalonItem = ({salon}) => {
    return (
       <li key={salon.name} className="w-full block pl-4 pr-[30px] text-left">
-         <a href="" className="flex border-b border-gray-200 py-4">
+         <Link to={`/${salon.salonId}`} className="flex border-b border-gray-200 py-4">
             <div className="pr-4">
                <p className="text-black text-[15px] leading-6 font-sans align-bottom">{salon.treatments.hair.nextAvailableTime}</p>
             </div>
             <div className="flex-1 pr-4">
                <h2 className="font-serif text-black text-xl leading-6">{salon.name}</h2>
                <div>
-                  <Rating rating={salon.rating} />
+                  <Rating rating={salon.rating} color="text-gray-700" font_size="11px" img_size="10" />
                </div>
                <p className="font-sans text-[15px] leading-5 font-light text-gray-700">{salon.info.address.street}</p>
             </div>
@@ -27,7 +28,7 @@ const ListSalonItem = ({salon}) => {
             <div className="relative -mr-2 self-center">
                <img src={chevron_left} className="w-2"/>
             </div>
-         </a>
+         </Link>
       </li>
    )
 }
