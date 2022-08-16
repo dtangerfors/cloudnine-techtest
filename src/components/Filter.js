4b7ba2bc2202bb45b2c082451c0dfbe4f1f98b2f
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-16 22:18:28 +0200

    Added range component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import InputRange from 'react-input-range';
+
+import "react-input-range/lib/css/index.css";
 
 import chevron_down from "../images/icons/chevron.down.svg";
 
-const Filter = ({ minValue = 25, maxValue = 50 }) => {
+const Filter = ({filterValue, setValue}) => {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="sticky z-10 top-[50px] h-14 flex justify-between p-4 bg-white border-b border-primary">
       <div className="font-sans font-light text-[15px]">
-        Price <span>${minValue}</span> - <span>${maxValue}</span>
+        Price <span>${filterValue.min}</span> - <span>${filterValue.max}</span>
       </div>
       <button
         onClick={() => setOpen(!open)}
@@ -19,7 +22,12 @@ const Filter = ({ minValue = 25, maxValue = 50 }) => {
         <img src={chevron_down} className="w-[14px]" />
       </button>
       <div className={`absolute z-10 w-full top-14 left-0 px-4 py-8 bg-white after:bg-gradient-to-b from-black/10 to-black/0 after:w-full after:h-32 after:block after:absolute after:left-0 after:-bottom-32 ${open ? 'block': 'hidden'}`}>
-         <input type="range" min="1" max="100" />
+         <InputRange 
+          maxValue={100}
+          minValue={1}
+          value={filterValue} 
+          onChange={value => setValue({isFiltered: true, ...value})}
+          />
       </div>
     </div>
   );
