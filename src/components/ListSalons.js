dafdfbc94b664191fc3d9ac49b23e597d116e9f3
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-16 22:13:09 +0200

    Added filter function

--- src/components/ListSalons.js
+++ src/components/ListSalons.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from "react";
 import ListSalonItem from "./ListSalonItem";
 
-const ListSalons = () => {
+const ListSalons = ({filterValue}) => {
    const [salons, setSalons] = useState([]);
+   const [filteredSalons, setFilteredSalons] = useState([]);
 
+   /* Fetch data from file and set it to states */ 
    const getData = () => {
       fetch('data.json', {
          headers: {
@@ -13,18 +15,38 @@ const ListSalons = () => {
       }).then(resp => {
          return resp.json()
       }).then(json => {
-         return setSalons(json.salons)
+         setSalons(json.salons);
+         setFilteredSalons(json.salons); 
       })
    }
 
+   /* When the filter range is active, filter 
+   the salons based on min and max value,
+   then set it to filtered state */
+   const filterSalons = () => {
+      if (filterValue.isFiltered) {
+        const salonsToFilter = [...salons];
+
+        const results = salonsToFilter.filter(salon => {
+         return salon.treatments.hair.price.amount >= filterValue.min && salon.treatments.hair.price.amount <= filterValue.max;
+        })
+
+        setFilteredSalons(results)
+      }
+   }
+
    useEffect(()=>{
-      getData()
+      getData();
    },[]);
+
+   useEffect(() => {
+      filterSalons();
+   },[filterValue]);
 
 
    return (
       <ul className="flex flex-col">
-         {salons.map(salon => {
+         {filteredSalons.map(salon => {
          return <ListSalonItem salon={salon} key={salon.name} />
       })}
       </ul>
