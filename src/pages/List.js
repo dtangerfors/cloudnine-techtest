985430c7bd6ea9bc7e2f653ba49ce78dd47d91ee
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-16 22:17:50 +0200

    Added state for filter values

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import Header from '../components/Header'
-import Filter from '../components/Filter'
-import ListSalons from '../components/ListSalons'
+import Header from "../components/Header";
+import Filter from "../components/Filter";
+import ListSalons from "../components/ListSalons";
 
 const List = () => {
-return (
-   <>
+  const [value, setValue] = useState({ min: 25, max: 50, isFiltered: false });
+  return (
+    <>
       <Header />
       <main className="pt-[50px]">
-         <Filter />
-         <ListSalons />
+        <Filter filterValue={value} setValue={setValue} />
+        <ListSalons filterValue={value} />
       </main>
-   </>
-)
-}
+    </>
+  );
+};
 
-export default List
+export default List;
