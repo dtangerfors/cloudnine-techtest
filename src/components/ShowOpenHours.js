426bddcefc65036d2b08ca034f5038646bd8b8e4
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-17 18:28:32 +0200

    Removed seconds

--- src/components/ShowOpenHours.js
+++ src/components/ShowOpenHours.js
@@ -19,13 +19,11 @@ const ShowOpenHours = ({openingHours}) => {
    let startDate = new Date(currentDate.getTime());
    startDate.setHours(startTime.split(":")[0]);
    startDate.setMinutes(startTime.split(":")[1]);
-   startDate.setSeconds(startTime.split(":")[2]);
 
    // Set endDate
    let endDate = new Date(currentDate.getTime());
    endDate.setHours(endTime.split(":")[0]);
    endDate.setMinutes(endTime.split(":")[1]);
-   endDate.setSeconds(endTime.split(":")[2]);
    
    // Compare if current time is between open hours
    let isOpen = startDate < currentDate && endDate > currentDate;
