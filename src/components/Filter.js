07a0a2cd1ef101294cabc9106060aa0cc16b2618
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-17 18:10:11 +0200

    Added onChangeComplete to limit re-renders

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,13 +1,23 @@
-import React, { useState, useCallback } from "react";
-import InputRange from 'react-input-range';
-
+import React, { useState } from "react";
+import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
 
 import chevron_down from "../images/icons/chevron.down.svg";
 
-const Filter = ({filterValue, setValue}) => {
+const Filter = ({ filterValue, setValue }) => {
   const [open, setOpen] = useState(false);
+  const [filterData, setFilterData] = useState({
+    min: filterValue.min,
+    max: filterValue.max,
+    isFiltered: false,
+  });
 
+  /*  Component to allow for filter based on a price range.
+      Uses ReactInputRance to get att good UX for end user. 
+      The min and max prices are set to its state when the 
+      range is changed. When user is finished and has released
+      the handle, the price range is sent to the state in the 
+      List view, this to prevent to many re-renders on the list. */
   return (
     <div className="sticky z-10 top-[50px] h-14 flex justify-between p-4 bg-white border-b border-primary">
       <div className="font-sans font-light text-[15px]">
@@ -17,17 +27,23 @@ const Filter = ({filterValue, setValue}) => {
         onClick={() => setOpen(!open)}
         type="button"
         className="w-14 h-14 -mr-4 -mt-4 grid place-items-center"
-        style={{transform: open ? 'rotate(180deg)' : 'rotate(0)'}}
+        style={{ transform: open ? "rotate(180deg)" : "rotate(0)" }}
       >
         <img src={chevron_down} className="w-[14px]" />
       </button>
-      <div className={`absolute z-10 w-full top-14 left-0 px-4 py-8 bg-white after:bg-gradient-to-b from-black/10 to-black/0 after:w-full after:h-32 after:block after:absolute after:left-0 after:-bottom-32 ${open ? 'block': 'hidden'}`}>
-         <InputRange 
+      <div
+        className={`absolute z-10 w-full top-14 left-0 p-8 bg-white after:bg-gradient-to-b from-black/10 to-black/0 after:w-full after:h-32 after:block after:absolute after:left-0 after:-bottom-32 ${
+          open ? "block" : "hidden"
+        }`}
+      >
+        <InputRange
+          formatLabel={(value) => `$${value}`}
           maxValue={100}
           minValue={1}
-          value={filterValue} 
-          onChange={value => setValue({isFiltered: true, ...value})}
-          />
+          value={filterData}
+          onChange={(value) => setFilterData({ isFiltered: true, ...value })}
+          onChangeComplete={() => setValue(filterData)}
+        />
       </div>
     </div>
   );
