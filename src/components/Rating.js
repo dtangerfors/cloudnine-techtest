25d3fa4dbeffb6a8983770a1eccdc5a73df47599
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-08-17 18:32:14 +0200

    Changed font class

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -5,12 +5,11 @@ import star_outlined from "../images/icons/star-outlined.svg";
 
 const Rating = ({ rating, color, font_size, img_size }) => {
 
-
   return (
    <div className="flex items-center">
     <div className="flex star-rating mr-1">
-      {/* Renderar ut fem stjarnor och kollar om score ar 
-      mindre an totalScore, om sa, valj den ifyllda ikonen. */}
+      {/* Renders five stars and sets the amount of stars
+        based on the rating from customers to filled */}
         {[...Array(5)].map((star, index) => {
           let starImg = star_outlined, score = index + 1;
           if (score <= rating.totalScore) starImg = star_filled;
